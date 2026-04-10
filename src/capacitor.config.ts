a820a35691f70e1370799c12a7e650844085d892
owner <owner@localhost>
2026-04-10 11:10:05 +0900

    android開発環境用設定

--- src/capacitor.config.ts
+++ src/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'myApp',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
