476af0b1cb95f7bdc30bf9ef9130f9a79af2445f
owner <owner@localhost>
2026-04-30 08:54:30 +0900

    ionic除去

--- src/src/main.ts
+++ src/src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
-
 import { auth } from '@/services/auth/auth';
 
 import './assets/css/app.css'
@@ -11,7 +9,6 @@ import './assets/css/app.css'
 auth.setupAuth();
 
 const app = createApp(App)
-  .use(IonicVue)
   .use(router);
 
 router.isReady().then(() => {
